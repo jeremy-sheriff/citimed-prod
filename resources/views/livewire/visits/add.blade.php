5049e6d77c5139f0b4ae0861c25d2f174ecfc276
Jeremy <muhohoweb@icloud.com>
2025-07-28 23:49:05 +0300

    Use dr morch build 1

--- resources/views/livewire/visits/add.blade.php
+++ resources/views/livewire/visits/add.blade.php
@@ -1,6 +1,5 @@
 <div class="w-full mx-auto px-4 sm:px-6 lg:px-8">
 
-
     <!-- Search Section -->
     <div class="bg-white dark:bg-gray-800 rounded-lg shadow p-6 mb-6">
         <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
